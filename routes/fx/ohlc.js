f5186495d1c3e20bb92c71b7b896c4ff65158612
Matheus Dadalto <mth_dadalto@hotmail.com>
2022-05-14 15:39:37 +0100

    Add /api/fx/ohlc/***/history endpoint to existing pairs

--- routes/fx/ohlc.js
+++ routes/fx/ohlc.js
@@ -4,6 +4,7 @@ var db = require(__basedir + "/db.js");
 
 //pair vwap SQL queries
 var getLastSingleVwapQuery = "SELECT `pair`, `vwap` FROM `market_price` WHERE `pair` LIKE ? ORDER BY `startTime` DESC LIMIT 1";
+var getHistoricalDateMinMaxQuery = "SELECT `pair`, date(`startTime`, 'unixepoch') AS 'startDate', max(`high`) AS 'max', min(`low`) AS 'min' FROM `market_price` WHERE `pair` LIKE ? GROUP BY `startDate` ORDER BY `startDate` ASC";
 
 //process a single pair vwap request
 function processLastSingleVwap(res, pair) {
@@ -16,14 +17,46 @@ function processLastSingleVwap(res, pair) {
     });
 }
 
-//ETHUSD pair vwap
+//process historical pair max and min on single dates request
+function processHistoricalDateMinMax(res, pair) {
+    db.all(getHistoricalDateMinMaxQuery, pair, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "err": err.message });
+            return;
+        }
+
+        var data = []
+        rows.forEach((row) => {
+            data.push([row.pair, row.startDate, row.max, row.min]);
+        });
+        res.json(data);
+    });
+}
+
+
+//SINGLES pair vwap
+
+//--- ETHUSD
 router.get('/ETHUSD', (req, res) => {
     processLastSingleVwap(res, "ETH/USD");
 });
 
-//ETHGBP pair vwap
+//--- ETHGBP
 router.get('/ETHGBP', (req, res) => {
     processLastSingleVwap(res, "ETH/GBP");
+});
+
+
+//HISTORICAL
+
+//--- ETHUSD 
+router.get('/ETHUSD/history', (req, res) => {
+    processHistoricalDateMinMax(res, "ETH/USD");
+});
+
+//--- ETHGBP
+router.get('/ETHGBP/history', (req, res) => {
+    processHistoricalDateMinMax(res, "ETH/GBP");
 });
 
 module.exports = router;
